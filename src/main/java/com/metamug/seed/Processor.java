908cf215fa794a006112db3a72f794717a773155
Deepak Mishra <d3ep4k@gmail.com>
2019-10-26 11:57:56 +0530

    Fixed dependency issue and removed jstl taglib import

--- src/main/java/com/metamug/seed/Processor.java
+++ src/main/java/com/metamug/seed/Processor.java
@@ -9,12 +9,7 @@ import com.metamug.entity.Request;
 import com.metamug.entity.Response;
 import com.metamug.exec.RequestProcessable;
 import com.metamug.seed.entity.Customer;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.Map;
-import javax.naming.Context;
-import javax.naming.InitialContext;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,19 +26,6 @@ public class Processor implements RequestProcessable {
     @Override
     public Response process(Request request, DataSource ds, Map<String, Object> args) throws Exception {
 
-        Context initialContext = new InitialContext();
-        DataSource datasource = (DataSource) initialContext.lookup("java:/comp/env/jdbc/mason");
-
-        try (Connection connection = datasource.getConnection()) {
-            Statement stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from movie");
-            while (rs.next()) {
-                System.out.println("Employee ID=" + rs.getFloat("rating") + ", Name=" + rs.getString("name"));
-            }
-        }
-        
-//        logger.info(request.getParameter(""));
-//        int id = Integer.parseInt(request.getParameter("id"));
 
         Customer customer = new Customer();
         customer.setName("John Doe.");
--- src/main/webapp/WEB-INF/resources/v1.0/execute.jsp
+++ src/main/webapp/WEB-INF/resources/v1.0/execute.jsp
@@ -1,14 +1,10 @@
-<jsp:directive.page pageEncoding="UTF-8" trimDirectiveWhitespaces="true"
-    errorPage="/WEB-INF/resources/error/500.jsp"/>
-<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@taglib uri="http://java.sun.com/jsp/jstl/sql" prefix="sql" %>
+<jsp:directive.page pageEncoding="UTF-8" trimDirectiveWhitespaces="true"  errorPage="/WEB-INF/resources/error/500.jsp"/>
 <%@taglib uri="mtg-mason.tld" prefix="m" %>
-
 
 <m:resource>
 
     <m:request method="GET">
-        <m:execute className="com.metamug.seed.Processor" var="executeResult" param="${mtgReq}" output="true"/>
+        <m:execute className="com.metamug.seed.Processor" var="getCustomer" param="${mtgReq}" output="true"/>
     </m:request>
 
 </m:resource>
